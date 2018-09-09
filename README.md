5ee6b2f9b670ff417a5db9b3b9aa615ee5a873d6
tofuchen <tofuchen@126.com>
2018-09-09 09:17:00 +0800

    有效的括号

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ leetcode题解
 * 9.[回文数](https://github.com/tofuchen94/leetcode/blob/master/src/main/java/com/leetcode/problem9/Solution.java)
 * 13.[罗马数字转整数](https://github.com/tofuchen94/leetcode/blob/master/src/main/java/com/leetcode/problem13/Solution.java)
 * 14.[最长公共前缀](https://github.com/tofuchen94/leetcode/blob/master/src/main/java/com/leetcode/problem14/Solution.java)
+* 20.[有效的括号](https://github.com/tofuchen94/leetcode/blob/master/src/main/java/com/leetcode/problem20/Solution.java)
---
+++ src/main/java/com/leetcode/problem20/Solution.java
@@ -0,0 +1,44 @@
+package com.leetcode.problem20;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * @author tofuchen
+ *
+ */
+public class Solution {
+	
+	public boolean isValid(String s) {
+		if(s==null||s.length()==0)return true;
+		Stack<Character> stack=new Stack<Character>();
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			if(ch=='('||ch=='{'||ch=='[') {
+				stack.push(ch);
+			}else if(ch==')') {
+				if(stack.isEmpty()||stack.peek()!='(') {
+					return false;
+				}else {
+					stack.pop();
+				}
+			}else if(ch=='}') {
+				if(stack.isEmpty()||stack.peek()!='{') {
+					return false;
+				}else {
+					stack.pop();
+				}
+			}else if(ch==']') {
+				if(stack.isEmpty()||stack.peek()!='[') {
+					return false;
+				}else {
+					stack.pop();
+				}
+			}
+		}
+		if(stack.isEmpty())return true;
+		return false;
+	}
+
+}
