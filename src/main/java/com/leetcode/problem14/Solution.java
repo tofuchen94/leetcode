d3e735fda827ef78f271e8db8e52f5aaddcf051b
tofuchen <tofuchen@126.com>
2018-09-09 09:07:29 +0800

    最长公共前缀

---
+++ src/main/java/com/leetcode/problem14/Solution.java
@@ -0,0 +1,24 @@
+package com.leetcode.problem14;
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，返回空字符串 ""。
+ * @author tofuchen
+ *
+ */
+public class Solution {
+	
+	public String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0)return "";
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<strs[0].length();i++) {
+        	char ch=strs[0].charAt(i);
+        	for(int j=0;j<strs.length;j++) {
+        		if(i>=strs[j].length()||strs[j].charAt(i)!=ch) {
+        			return res.toString();
+        		}
+        	}
+        	res.append(ch);
+        }
+        return res.toString();
+    }
+
+}
